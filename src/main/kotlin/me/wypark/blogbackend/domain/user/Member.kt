519746f663dd6fe569851a05e764798c24074091
wypark <wyp9679@gmail.com>
2025-06-19 11:53:35 +0900

    reconstructed: add member (1/2)
    Source unit: src/main/kotlin/me/wypark/blogbackend/domain/user/Member.kt:1-24 of 48.

---
+++ src/main/kotlin/me/wypark/blogbackend/domain/user/Member.kt
@@ -0,0 +1,24 @@
+package me.wypark.blogbackend.domain.user
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+import me.wypark.blogbackend.domain.common.BaseTimeEntity
+
+@Entity
+@Table(name = "member")
+class Member(
+    @Column(nullable = false, unique = true)
+    val email: String,
+
+    @Column(nullable = false)
+    val password: String,
+
+    @Column(nullable = false)
+    val nickname: String,
+
