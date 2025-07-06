d5272f0e19e7b3d0ea94d4f1dd9064608eb4ce57
wypark <wyp9679@gmail.com>
2025-07-06 17:33:46 +0900

    reconstructed: add post view daily stats (1/2)
    Source unit: src/main/kotlin/me/wypark/blogbackend/domain/post/PostViewDailyStats.kt:1-16 of 49.

---
+++ src/main/kotlin/me/wypark/blogbackend/domain/post/PostViewDailyStats.kt
@@ -0,0 +1,16 @@
+package me.wypark.blogbackend.domain.post
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Index
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.Table
+import jakarta.persistence.UniqueConstraint
+import me.wypark.blogbackend.domain.common.BaseTimeEntity
+import java.time.LocalDate
+
