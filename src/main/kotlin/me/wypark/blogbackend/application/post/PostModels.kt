d16b2c3f1033635e9544e11db257685d228e6831
wypark <wyp9679@gmail.com>
2025-08-07 13:34:30 +0900

    reconstructed: add post models (1/3)
    Source unit: src/main/kotlin/me/wypark/blogbackend/application/post/PostModels.kt:1-40 of 89.

---
+++ src/main/kotlin/me/wypark/blogbackend/application/post/PostModels.kt
@@ -0,0 +1,40 @@
+package me.wypark.blogbackend.application.post
+
+import me.wypark.blogbackend.domain.post.Post
+import me.wypark.blogbackend.domain.post.PostSummary
+import java.time.LocalDateTime
+
+data class PostNeighborResponse(
+    val slug: String,
+    val title: String
+) {
+    companion object {
+        fun from(post: Post) = PostNeighborResponse(post.slug, post.title)
+    }
+}
+
+data class PostResponse(
+    val id: Long,
+    val title: String,
+    val content: String,
+    val slug: String,
+    val categoryName: String?,
+    val viewCount: Long,
+    val createdAt: LocalDateTime,
+    val prevPost: PostNeighborResponse?,
+    val nextPost: PostNeighborResponse?
+) {
+    companion object {
+        fun from(post: Post, previous: Post? = null, next: Post? = null): PostResponse {
+            return PostResponse(
+                id = requireNotNull(post.id) { "Persisted post must have an id" },
+                title = post.title,
+                content = post.content,
+                slug = post.slug,
+                categoryName = post.category?.name,
+                viewCount = post.viewCount,
+                createdAt = post.createdAt,
+                prevPost = previous?.let(PostNeighborResponse::from),
+                nextPost = next?.let(PostNeighborResponse::from)
+            )
+        }
