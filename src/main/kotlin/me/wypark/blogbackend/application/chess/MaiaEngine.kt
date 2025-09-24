de95253b935f83d37da86840481740a48712cf7b
wypark <wyp9679@gmail.com>
2025-09-24 14:37:58 +0900

    reconstructed: add maia engine (1/2)
    Source unit: src/main/kotlin/me/wypark/blogbackend/application/chess/MaiaEngine.kt:1-27 of 43.

---
+++ src/main/kotlin/me/wypark/blogbackend/application/chess/MaiaEngine.kt
@@ -0,0 +1,27 @@
+package me.wypark.blogbackend.application.chess
+
+interface MaiaEngine {
+
+    fun getState(request: MaiaStateRequest): MaiaStateResponse
+
+    fun playMove(request: MaiaPlayRequest): MaiaPlayResponse
+}
+
+data class MaiaStateRequest(
+    val moves: List<String>,
+    val white: String? = null,
+    val black: String? = null,
+    val event: String = "Maia3"
+)
+
+data class MaiaPlayRequest(
+    val moves: List<String>,
+    val rating: Int,
+    val model: String,
+    val temperature: Double,
+    val topP: Double,
+    val white: String? = null,
+    val black: String? = null,
+    val event: String = "Maia3"
+)
+
