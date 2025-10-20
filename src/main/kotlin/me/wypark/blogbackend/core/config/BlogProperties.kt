c6d09a970d0090b807318aa60ce8566c391626b0
wypark <wyp9679@gmail.com>
2025-10-20 17:54:54 +0900

    reconstructed: add blog properties (2/2)
    Source unit: src/main/kotlin/me/wypark/blogbackend/core/config/BlogProperties.kt:17-41 of 41.

--- src/main/kotlin/me/wypark/blogbackend/core/config/BlogProperties.kt
+++ src/main/kotlin/me/wypark/blogbackend/core/config/BlogProperties.kt
@@ -14,3 +14,28 @@ data class CorsProperties(
     val allowedOrigins: List<String> = listOf("https://blog.wypark.me")
 )
 
+@ConfigurationProperties("spring.cloud.aws")
+data class AwsProperties(
+    val credentials: Credentials = Credentials(),
+    val region: AwsRegion = AwsRegion(),
+    val s3: S3 = S3()
+) {
+    data class Credentials(
+        val accessKey: String = "admin",
+        val secretKey: String = "password"
+    )
+
+    data class AwsRegion(
+        val static: String = "ap-northeast-2"
+    )
+
+    data class S3(
+        val endpoint: String = "http://minio:9000",
+        val bucket: String = "blog-images"
+    )
+}
+
+@ConfigurationProperties("blog.image")
+data class ImageProperties(
+    val initializeBucket: Boolean = true
+)
