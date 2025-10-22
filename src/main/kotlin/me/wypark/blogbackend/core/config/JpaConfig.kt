c62c3158ab102a5a6cadff506c2b80571cfab818
wypark <wyp9679@gmail.com>
2025-10-22 14:55:49 +0900

    reconstructed: add jpa config
    Source unit: src/main/kotlin/me/wypark/blogbackend/core/config/JpaConfig.kt:1-8 of 8.

---
+++ src/main/kotlin/me/wypark/blogbackend/core/config/JpaConfig.kt
@@ -0,0 +1,8 @@
+package me.wypark.blogbackend.core.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+
+@Configuration
+@EnableJpaAuditing
+class JpaConfig
