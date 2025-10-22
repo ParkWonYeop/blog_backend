61860dc21a88955a938d0f3b0ebfbd2c7deb9450
wypark <wyp9679@gmail.com>
2025-10-22 17:33:18 +0900

    reconstructed: add maia config
    Source unit: src/main/kotlin/me/wypark/blogbackend/core/config/MaiaConfig.kt:1-35 of 35.

---
+++ src/main/kotlin/me/wypark/blogbackend/core/config/MaiaConfig.kt
@@ -0,0 +1,35 @@
+package me.wypark.blogbackend.core.config
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.client.SimpleClientHttpRequestFactory
+import org.springframework.web.client.RestClient
+import java.time.Duration
+
+@Configuration
+@EnableConfigurationProperties(MaiaProperties::class)
+class MaiaConfig {
+
+    @Bean
+    fun maiaRestClient(
+        builder: RestClient.Builder,
+        properties: MaiaProperties
+    ): RestClient {
+        val requestFactory = SimpleClientHttpRequestFactory().apply {
+            setConnectTimeout(Duration.ofSeconds(5))
+            setReadTimeout(Duration.ofSeconds(120))
+        }
+        return builder
+            .baseUrl(properties.engineUrl)
+            .requestFactory(requestFactory)
+            .build()
+    }
+}
+
+@ConfigurationProperties(prefix = "maia")
+data class MaiaProperties(
+    val engineUrl: String = "http://localhost:8000",
+    val gameSessionTtl: Duration = Duration.ofHours(6)
+)
