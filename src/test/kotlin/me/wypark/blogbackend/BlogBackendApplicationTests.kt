497aee6f689b50f7832872af93e9280bcf692ecb
wypark <wyp9679@gmail.com>
2026-01-08 17:03:35 +0900

    reconstructed: cover blog backend application tests
    Source unit: src/test/kotlin/me/wypark/blogbackend/BlogBackendApplicationTests.kt:1-15 of 15.

---
+++ src/test/kotlin/me/wypark/blogbackend/BlogBackendApplicationTests.kt
@@ -0,0 +1,15 @@
+package me.wypark.blogbackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+
+@SpringBootTest
+@ActiveProfiles("test")
+class BlogBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
